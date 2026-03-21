66d0a5d83e83936cd742aa8ece9e278c06d6b2ae
GalactoseGalaxy <hayden.m.farris@gmail.com>
2026-03-21 13:21:25 -0700

    Project Start!

---
+++ src/popup/Popup.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Popup: React.FC = () => {
+  return (
+    <div style={{ width: 300, padding: 16 }}>
+      <h1>My Extension</h1>
+      <p>Hello from your React popup!</p>
+    </div>
+  )
+}
+
+export default Popup
